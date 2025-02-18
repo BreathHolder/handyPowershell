0d9e05cdaebac7478eeeb6421f1acb955b4d7cd0
Mat Sturdevant <matty.ise@outlook.com>
2025-02-17 20:45:19 -0500

    change resolution measurements to variables for easier modification by other users.

--- FixResolution.ps1
+++ FixResolution.ps1
@@ -1,2 +1,7 @@
-Start-Process -FilePath "C:\Tools\NirCmd\nircmd.exe" -ArgumentList "setdisplay monitor:0 2560 1440 32" -Wait
-Start-Process -FilePath "C:\Tools\NirCmd\nircmd.exe" -ArgumentList "setdisplay monitor:1 2560 1440 32" -Wait
+$monitorWidth1 = 2560
+$monitorHeight1 = 1440
+$monitorWidth2 = 2560
+$monitorHeight2 = 1440
+
+Start-Process -FilePath "C:\Tools\NirCmd\nircmd.exe" -ArgumentList "setdisplay monitor:0 $monitorWidth1 $monitorHeight1 32" -Wait
+Start-Process -FilePath "C:\Tools\NirCmd\nircmd.exe" -ArgumentList "setdisplay monitor:1 $monitorWidth2 $monitorHeight2 32" -Wait
